97cf3a7e7c1ebb70398835bbce191ffa5b9d897d
Tonysysop <145222227+Tonysysop@users.noreply.github.com>
2025-03-08 21:32:35 +0100

    Page structure (#2)
    * Page setup and structure
    
    * Page setup and structure

---
+++ Tinu-taskmanager/src/components/TaskCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Tag from '@/components/Tag'
+import DeleteIcon from '../assets/delete.png'
+
+const TaskCard = () => {
+  return (
+    <article className='w-full min-h-[100px] border border-gray-300 rounded-lg p-4 my-4'> {/*Task_card*/}
+        <p className='text-lg font-semibold mb-3'>This is sample text</p>{/*taskTest*/}
+
+
+        <div className='flex items-center justify-between'>{/*task_card_bottom_line*/}
+           <div>
+                <Tag tagName="School" />
+                <Tag tagName="Household" />
+                <Tag tagName="Others" />
+                <Tag tagName="Personal" />
+           </div>{/*task_card_tag*/}
+
+           <div className='w-[35px] h-[35px] rounded-full flex items-center justify-center cursor-pointer transition-all duration-300 ease-in-out hover:bg-gray-200'>
+                <img className='w-[20px] opacity-50 transition-all duration-300 ease-in-out hover:opacity-80' src={DeleteIcon} alt="" /> {/*Delete icon*/}
+           </div> {/*task_delete*/}
+        </div>
+    </article>
+  )
+}
+
+export default TaskCard
---
+++ Tinu-taskmanager/src/components/TaskColumn.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TaskCard from '@/components/TaskCard';
+
+const TaskColumn = ({icon, columnName}:{ icon: string; columnName: string }) => {
+  return (
+    <section className='w-1/3  m-5'>
+        <h2 className='flex items-center font-bold'>
+            <img className='w-[30px] mr-[5px]' src={icon} alt="" /> {columnName}
+        </h2> {/*Task_column_heading and task_column icon*/} 
+
+
+        <TaskCard />
+    </section>
+  )
+}
+
+export default TaskColumn

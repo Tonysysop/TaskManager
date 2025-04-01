41d42337762dead9cc73aa6626519ac5e9905c56
Tonysysop <145222227+Tonysysop@users.noreply.github.com>
2025-04-01 15:31:17 +0100

    Task rendering (#7)
    * task card rendering
    
    * task card rendering
    
    * task card rendering

--- Tinu-taskmanager/src/App.tsx
+++ Tinu-taskmanager/src/App.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+
+import  { useState } from 'react'
 import TaskForm from './components/TaskForm'
 import TaskColumn from './components/TaskColumn'
 import TodoIcon from "./assets/direct-hit.png"
 import DoingIcon from "./assets/glowing-star.png"
 import DoneIcon from "./assets/check-mark-button.png"
 
-interface TaskComponent {
+
+interface TaskComponent { 
   task: string;
   status: string;
   tags: string[];
